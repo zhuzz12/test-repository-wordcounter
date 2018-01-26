0d3c8792c1a7a7fc41fa7b3f54134a45008ff325
Zhuzz12 <zhuldyzay.aitakhunova@nu.edu.kz>
2018-01-26 09:26:39 +0600

    Add implementation of the Wordcount method All tests passed

--- src/main/java/com/example/wordcounter/App.java
+++ src/main/java/com/example/wordcounter/App.java
@@ -1,5 +1,7 @@
 package com.example.wordcounter;
 
+import java.util.ArrayList;
+
 public class App 
 {
     public static void main( String[] args )
@@ -8,6 +10,18 @@ public class App
     }
     
     public static int wordcount(String s) {
-        return 0;
+    	
+    	String[] splittedText = s.split("[\\n\\s.,!:;=]");
+		ArrayList<String> words = new ArrayList<String>(); 
+		
+		for (int i = 0; i < splittedText.length; i++) {
+			if (!splittedText[i].equals("")){
+				words.add(splittedText[i]);
+			}
+		}
+		
+		//System.out.println(words.toString());
+		
+		return words.size();
     }
 }
